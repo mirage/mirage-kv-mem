a6a732921fe8f472ff56a8b13bfeda6f3179cd4b
linse <sschirme@gmail.com>
2019-02-12 10:19:56 +0100

    Conform to the new Mirage-kv.RW interface

--- src/mirage_fs_mem.ml
+++ src/mirage_fs_mem.ml
@@ -1,7 +1,7 @@
-type write_error = [ Mirage_fs.write_error | `Directory_not_empty ]
+type write_error = [ Mirage_kv.write_error | `Directory_not_empty ]
 
 let pp_write_error ppf = function
-  | #Mirage_fs.write_error as e -> Mirage_fs.pp_write_error ppf e
+  | #Mirage_kv.write_error as e -> Mirage_kv.pp_write_error ppf e
   | `Directory_not_empty -> Fmt.string ppf "directory not empty"
 
 module Pure = struct
@@ -12,125 +12,88 @@ module Pure = struct
 
   type t =
    | Directory of t M.t
-   | File of Cstruct.t
+   | File of string
+
+  type key = Mirage_kv.Key.t
 
   let empty () = Directory M.empty
 
-  let get_segs p =
-    let p' = Astring.String.trim ~drop:(function '/' -> true | _ -> false) p in
-    match p' with
-    | "" -> [] (* this represents /, the root of the file system *)
-    | path -> Fpath.(segs (v path))
-
-  let find_file_or_directory t path =
-    let segs = get_segs path in
+  let find_file_or_directory t key =
     let rec find t = function
       | [] -> Ok t
       | hd::tl -> match t with
-        | File _xs -> Error `Not_a_directory
+        | File _xs -> Error (`Dictionary_expected key)
         | Directory m ->
           match M.find_opt hd m with
           | Some t' -> find t' tl
-          | None -> Error `No_directory_entry
+          | None -> Error (`Not_found key)
     in
-    find t segs
+    find t (Mirage_kv.Key.segments key)
 
-  let add_file_or_directory ?(overwrite = false) t path value =
-    let segs = get_segs path in
+  let add_file_or_directory t key value =
     let rec add t' = function
       | [] -> Ok value
       | [x] ->
         begin match t' with
-          | File _ -> Error `Not_a_directory
-          | Directory m ->
-            (match M.find_opt x m with
-             | None -> Ok (M.add x value m)
-             | Some _ when overwrite -> Ok (M.add x value m)
-             | Some _ -> Error `File_already_exists) >>= fun m' ->
-            Ok (Directory m')
+          | File _ -> Error (`Dictionary_expected key)
+          | Directory m -> Ok (Directory (M.add x value m))
         end
       | hd::tl ->
         begin
           match t' with
-          | File _ -> Error `Not_a_directory
+          | File _ -> Error (`Dictionary_expected key)
           | Directory m ->
-            (match M.find_opt hd m with
-              | None -> Error `No_directory_entry
-              | Some t'' -> Ok t'') >>= fun node ->
+            let node = match M.find_opt hd m with
+              | None -> Directory M.empty
+              | Some t'' -> t''
+            in
             add node tl >>= fun t''' ->
             let m' = M.add hd t''' m in
             Ok (Directory m')
         end
     in
-    add t segs
+    add t (Mirage_kv.Key.segments key)
 
-  let remove_file_or_directory t path =
-    find_file_or_directory t path >>= (function
-        | File _ -> Ok ()
-        | Directory m when M.is_empty m -> Ok ()
-        | _ -> Error `Directory_not_empty) >>= fun () ->
-    let segs = get_segs path in
+  let remove_file_or_directory t key =
     let rec remove t = function
-      | [] -> Error `Not_a_directory
+      | [] -> Ok (Directory M.empty)
       | [x] -> begin match t with
-          | File _ -> Error `Not_a_directory
+          | File _ -> Error (`Dictionary_expected key)
           | Directory m ->
             let m' = M.remove x m in
             Ok (Directory m')
         end
       | hd::tl -> match t with
-        | File _ -> Error `Not_a_directory
+        | File _ -> Error (`Dictionary_expected key)
         | Directory m ->
           (match M.find_opt hd m with
-           | None -> Error `Not_a_directory
+           | None -> Error (`Dictionary_expected key)
            | Some t' -> Ok t') >>= fun node ->
           remove node tl >>| fun t' ->
           let m' = M.add hd t' m in
           Directory m'
     in
-    remove t segs
+    remove t (Mirage_kv.Key.segments key)
 
   (* TODO bounds check*)
-  let read (t : t) path offset length =
-    find_file_or_directory t path >>= function
-    | Directory _ -> Error `Is_a_directory
-    | File value ->
-      let read_length = min length (Cstruct.len value - offset) in
-      Ok [Cstruct.sub value offset read_length]
-
-  let size (t : t) path =
-    find_file_or_directory t path >>= function
-    | Directory _ -> Error `Is_a_directory
-    | File value -> Ok (Int64.of_int @@ Cstruct.len value)
-
-  let create t path = add_file_or_directory t path (File Cstruct.empty)
-  let mkdir t path = add_file_or_directory t path (empty ())
+  let read (t : t) key =
+    find_file_or_directory t key >>= function
+    | Directory _ -> Error (`Value_expected key)
+    | File value -> Ok value
 
   let destroy (t : t) path = remove_file_or_directory t path
 
-  let stat (t : t) path =
-    find_file_or_directory t path >>= function
-    | Directory _ -> Ok Mirage_fs.{ filename = path ; read_only = false ; directory = true ; size = 0L }
-    | File value -> Ok Mirage_fs.{ filename = path ; read_only = false ; directory = false ; size = Int64.of_int @@ Cstruct.len value }
+  let list (t : t) key =
+    find_file_or_directory t key >>= function
+    | File _ -> Error (`Dictionary_expected key)
+    | Directory m -> Ok (List.map (fun (key, value) -> key, match value with File _ -> `Value | Directory _ -> `Dictionary) @@ M.bindings m)
 
-  let listdir (t : t) path =
-    find_file_or_directory t path >>= function
-    | File _ -> Error `Not_a_directory
-    | Directory m -> Ok (fst @@ List.split @@ M.bindings m)
-
-  let write (t : t) path offset data =
-    if offset <> 0
-    then Error `No_space
-    else
-      match find_file_or_directory t path with
-      | Error `No_directory_entry -> add_file_or_directory t path (File data)
-      | Error e -> Error e
-      | Ok (Directory _) -> Error `Is_a_directory
-      | Ok (File _) -> add_file_or_directory ~overwrite:true t path (File data)
+  let set (t : t) key data =
+    add_file_or_directory t key (File data)
 
   let pp fmt t =
     let rec pp_things ?(prefix = "") () fmt = function
-      | File v -> Fmt.pf fmt "File %s %d: %s@." prefix (Cstruct.len v) (Cstruct.to_string v)
+      | File v -> Fmt.pf fmt "File %s %d: %s@." prefix (String.length v) v
       | Directory m ->
         List.iter (fun (k, v) ->
             pp_things ~prefix:(prefix ^ "/" ^ k) () fmt v)
@@ -139,46 +102,40 @@ module Pure = struct
     pp_things () fmt t
 
   let rec equal t t' = match t, t' with
-    | File v, File v' -> Cstruct.equal v v'
+    | File v, File v' -> String.equal v v'
     | Directory m, Directory m' -> M.equal equal m m'
     | _ -> false
 
 end
 
-type error = Mirage_fs.error
-let pp_error = Mirage_fs.pp_error 
+type error = Mirage_kv.error
+let pp_error = Mirage_kv.pp_error 
 
 type +'a io = 'a Lwt.t
-type page_aligned_buffer = Cstruct.t
+type value = string
+type key = Mirage_kv.Key.t
+
+let last_modified _ _ = assert false
+let digest _ _ = assert false
+let batch _ ?retries:_ _ = assert false
+let exists _ _ = assert false
 
 let connect _s = Lwt.return (ref (Pure.empty ()))
 let disconnect _t = Lwt.return ()
 
 type t = Pure.t ref
 
-let read m path offset length = Lwt.return @@ Pure.read !m path offset length
+let get m path = Lwt.return @@ Pure.read !m path
 
-let size m path = Lwt.return @@ Pure.size !m path
-
-let create m path = Lwt.return @@ match Pure.create !m path with
+let remove m path = Lwt.return @@ match Pure.destroy !m path with
   | Error e -> Error e
   | Ok m' -> m := m'; Ok ()
 
-let mkdir m path = Lwt.return @@ match Pure.mkdir !m path with
+let list m path = Lwt.return @@ Pure.list !m path
+
+let set m path data = Lwt.return @@ match Pure.set !m path data with
   | Error e -> Error e
   | Ok m' -> m := m'; Ok ()
-
-let destroy m path = Lwt.return @@ match Pure.destroy !m path with
-  | Error e -> Error e
-  | Ok m' -> m := m'; Ok ()
-
-let stat m path = Lwt.return @@ Pure.stat !m path
-
-let listdir m path = Lwt.return @@ Pure.listdir !m path
-
-let write m path offset data = Lwt.return @@ match Pure.write !m path offset data with
-  | Error e -> Error e
-  | Ok m' -> m := m'; Ok () 
 
 let pp fmt m = Pure.pp fmt !m
 
--- src/mirage_fs_mem.mli
+++ src/mirage_fs_mem.mli
@@ -1,23 +1,21 @@
-type write_error = [ Mirage_fs.write_error | `Directory_not_empty ]
-type error = Mirage_fs.error
+type write_error = [ Mirage_kv.write_error | `Directory_not_empty ]
+type error = Mirage_kv.error
 
 module Pure : sig
   type t
+  type key = Mirage_kv.Key.t
   val empty : unit -> t
-  val read : t -> string -> int -> int -> (Cstruct.t list, Mirage_fs.error) result
-  val size : t -> string -> (int64, Mirage_fs.error) result
-  val create : t -> string -> (t, write_error) result
-  val mkdir : t -> string -> (t, write_error) result
-  val destroy : t -> string -> (t, write_error) result
-  val stat : t -> string -> (Mirage_fs.stat, Mirage_fs.error) result
-  val listdir : t -> string -> (string list, Mirage_fs.error) result
-  val write : t -> string -> int -> Cstruct.t -> (t, write_error) result
+  val read : t -> key -> (string, error) result
+  val destroy : t -> key -> (t, write_error) result
+  val list : t -> key -> ((string * [`Value | `Dictionary]) list, error) result
+
+  val set : t -> key -> string -> (t, write_error) result
 
   val equal : t -> t -> bool
   val pp : t Fmt.t
 end
 
-include Mirage_fs_lwt.S
+include Mirage_kv_lwt.RW
   with type write_error := write_error
    and type error := error
 
